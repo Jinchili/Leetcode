b542701dd92ba75ae7f56f2b22ff00f8ae58eadd
Jinchili <39415889+Jinchili@users.noreply.github.com>
2018-07-09 22:17:00 -0500

    summary of binary_search_practice trick

---
+++ binary_search.py
@@ -0,0 +1,7 @@
+##definition:binary tree: left nodes <right nodes:
+##space complexicity O(N),
+##second level:time and space complexity
+##approach summary
+##similar problem
+##merge sort vs quick sort
+##
