3e919cfabd8ee9b58a71f1ee215e92d06fb9f868
Jinchili <39415889+Jinchili@users.noreply.github.com>
2018-07-09 22:25:52 -0500

    binary_search_practice trick

--- binary_search.py
+++ binary_search.py
@@ -1,7 +1,16 @@
 ##definition:binary tree: left nodes <right nodes:
 ##space complexicity O(N),
+####
 ##second level:time and space complexity
 ##approach summary
 ##similar problem
 ##merge sort vs quick sort
-##
+
+
+##clarify
+##appeoach
+##input/output/helper reference to pass through
+##assumption:corner case/edge case/base case
+##comment + code + review
+##time complexity
+##space complexity
